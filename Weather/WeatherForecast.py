bf1ec86e83766a23cf3cdb0fb72b8871f02a83f6
kanutope <16442099+kanutope@users.noreply.github.com>
2021-02-17 16:04:34 +0100

    Added 'config.json' to de-hardcode a few things Moved some functions into WeatherForecast.py

--- Weather/WeatherForecast.py
+++ Weather/WeatherForecast.py
@@ -16,6 +16,8 @@ means.
 
 
 import json
+import os
+import re
 
 import requests
 import time as tm
@@ -28,6 +30,7 @@ class class_Locations:
     def __init__(self):
         with open("locations.json", "r") as fp:
             self.locations = json.loads(fp.read())
+            fp.close()
 
     def getLocation(self, code):
         return self.locations[code]
@@ -41,11 +44,13 @@ class class_APIconfig:
         self.keys = {}
         with open("APIconfig.json", "r") as fp:
             self.config = json.loads(fp.read())
+            fp.close()
 
     def __getAPIkey(self, prov):
         if not (prov in list(self.keys)):
             with open(self.config[prov]["keyfile"], "r") as fp:
                 self.keys[prov] = fp.readline().replace("\n", "")
+                fp.close()
 
         return self.keys[prov]
 
@@ -63,8 +68,28 @@ class class_APIconfig:
         return list(self.config[prov]["reports"])
 
 
-__APIconfig = class_APIconfig()
+class class_Config:
+    """ __init__()
+        ----------
+    """
+    def __init__(self):
+        with open("config.json", "r") as fp:
+            self.config = json.loads(fp.read())
+            fp.close()
+
+    def getOutputDir(self):
+        return self.config["output"]
+
+    def getQueryDir(self):
+        return self.config["query"]
+
+    def getStructDir(self):
+        return self.config["struct"]
+
+
+__Config = class_Config()
 __Locations = class_Locations()
+__APIconfig = class_APIconfig()
 
 
 def listProviders():
@@ -73,6 +98,18 @@ def listProviders():
 
 def listReports(prov):
     return __APIconfig.listReports(prov)
+
+
+def getOutputDir():
+    return __Config.getOutputDir()
+
+
+def getQueryDir():
+    return __Config.getQueryDir()
+
+
+def getStructDir():
+    return __Config.getStructDir()
 
 
 WINDDIR = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
@@ -115,26 +152,107 @@ def fetchAPI(code, prov, rep) -> dict:
 def fetchFile(fnam) -> dict:
     with open(fnam, "r") as fp:
         response = json.loads(fp.read())
+        fp.close()
+
         return response
 
 
-def loop(data=None):
-    if data is None:
-        data = fetchFile()
+def structDict(fp, obj, ind=""):
+    ind = ind + "  "
+    for key in iter(obj):
+        typ = type(obj[key])
+        print(ind, key, typ, file=fp)
 
-    for rec in data["list"]:
-        main = rec["main"]
-        wthr = rec["weather"]
-        wind = rec["wind"]
+        if typ in iter(functions):
+            functions[typ](fp, obj[key], ind)
 
-        indx = int(((wind["deg"] + WINDRNG / 2.0) % 360) / WINDRNG)
-        knts = wind["speed"] * MPS2KNT
-        bft = beaufort(knts)
 
-        time = tm.strftime("%a %d %b %H:%M", tm.localtime(rec['dt']))
-        out = f"{time} - T:{main['temp']:5.1f} gevoels {main['feels_like']:5.1f}" \
-              f" [{main['temp_min']:5.1f} ~{main['temp_max']:5.1f}]" \
-              f" - P: {main['pressure']:4d}hPa" \
-              f" - W: {knts:2.0f}kn {bft:2d}bft {WINDDIR[indx]:^3s} {WINDDESC[bft]:s}"
+def structList(fp, obj, ind=""):
+    ind = ind + "  "
+    for fld in obj:
+        typ = type(fld)
+        print(ind, typ, file=fp)
 
-        print(out)
+        if typ in iter(functions):
+            functions[typ](fp, fld, ind)
+
+
+functions = {dict: structDict,
+             list: structList
+             }
+
+
+def getLatest(loc="", prov="", rep=""):
+    if len(loc) > 0:
+        patt = f"{loc}"
+    else:
+        patt = "[0-9,A-Z,a-z]{4}"
+
+    if len(prov) > 0:
+        patt = f"{patt}_{prov}"
+    else:
+        patt = f"{patt}_[A-Z,a-z]+"
+
+    if len(rep) > 0:
+        patt = f"{patt}\\-{rep}"
+    else:
+        patt = f"{patt}\\-[A-Z,a-z]*"
+
+    patt = re.compile(patt)
+
+    prev = ""
+    mask = ""
+    last = []
+    for f in [f for f in sorted(os.listdir(getOutputDir())) if patt.search(f) is not None]:
+        curr = re.sub(r"_[0-9]{8}-[0-9]{4}.json", "", f)
+
+        if mask == "":
+            mask = curr
+        else:
+            if curr != mask:
+                if len(mask) > 0:
+                    last.append(prev)
+                mask = curr
+
+        prev = f
+
+    if len(prev) > 0:
+        last.append(prev)
+
+    return last
+
+
+def getStructure(loc="", prov="", rep=""):
+    for f in getLatest(loc, prov, rep):
+        inpFile = getOutputDir() + "/" + f
+        outFile = inpFile.replace(f"{getOutputDir()}/", f"{getStructDir()}/").replace(".json", ".txt")
+        resp = (fetchFile(inpFile))
+
+        with open(outFile, "w") as fp:
+            typ = type(resp)
+            print("response", typ, file=fp)
+
+            functions[typ](fp, resp)
+            fp.close()
+
+
+def loop(loc="", prov="", rep=""):
+    for fil in getLatest(loc, prov, rep):
+        data = fetchFile(getOutputDir() + "/" + fil)
+
+        for rec in data["list"]:
+            main = rec["main"]
+            wthr = rec["weather"]
+            wind = rec["wind"]
+
+            indx = int(((wind["deg"] + WINDRNG / 2.0) % 360) / WINDRNG)
+            knts = wind["speed"] * MPS2KNT
+            bft = beaufort(knts)
+
+            time = tm.strftime("%a %d %b %H:%M", tm.localtime(rec['dt']))
+            out = f"{time} - T:{main['temp']:5.1f} gevoels {main['feels_like']:5.1f}" \
+                  f" [{main['temp_min']:5.1f} ~{main['temp_max']:5.1f}]" \
+                  f" - P: {main['pressure']:4d}hPa" \
+                  f" - W: {knts:2.0f}kn {bft:2d}bft {WINDDIR[indx]:^3s} {WINDDESC[bft]:s}"
+
+            print(out)
--- Weather/main.py
+++ Weather/main.py
@@ -13,11 +13,9 @@ means.
 import WeatherForecast as wf
 import time as tm
 import json
-import os
-import re
 
 
-def printReport(loc, prov, rep):
+def fetch_and_print(loc, prov, rep):
     print(loc, prov, rep)
     print(wf.fetchAPI(loc, prov, rep))
 
@@ -26,7 +24,7 @@ def dumpReport(loc, prov, rep):
     resp = wf.fetchAPI(loc, prov, rep)
     if len(resp) > 0:
         ts = tm.strftime("%Y%m%d-%H%M", tm.localtime())
-        nam = f"output/{loc}_{prov}-{rep}_{ts}.json"
+        nam = f"{wf.getOutputDir()}/{loc}_{prov}-{rep}_{ts}.json"
         with open(nam, "w") as fp:
             print(ts, loc, prov, rep, json.dump(resp, fp, indent=2))
             fp.close()
@@ -45,87 +43,12 @@ def execute(func, loc, prov="", rep=""):
             func(loc, prov, rep)
 
 
-def latest(loc="", prov="", rep=""):
-    if len(loc) > 0:
-        patt = f"{loc}"
-    else:
-        patt = "[0-9,A-Z,a-z]{4}"
-
-    if len(prov) > 0:
-        patt = f"{patt}_{prov}"
-    else:
-        patt = f"{patt}_[A-Z,a-z]+"
-
-    if len(rep) > 0:
-        patt = f"{patt}\\-{rep}"
-    else:
-        patt = f"{patt}\\-[A-Z,a-z]*"
-
-    patt = re.compile(patt)
-
-    prev = ""
-    mask = ""
-    last = []
-    for f in [f for f in sorted(os.listdir("output/")) if patt.search(f) is not None]:
-        curr = re.sub(r"_[0-9]{8}\-[0-9]{4}.json", "", f)
-
-        if mask == "":
-            mask = curr
-        else:
-            if curr != mask:
-                if len(mask) > 0:
-                    last.append(prev)
-                mask = curr
-
-        prev = f
-
-    if len(prev) > 0:
-        last.append(prev)
-
-    return last
-
-
-def structDict(fp, obj, ind=""):
-    ind = ind + "  "
-    for key in iter(obj):
-        typ = type(obj[key])
-        print(ind, key, typ, file=fp)
-        
-        if typ in iter(functions):
-            functions[typ](fp, obj[key], ind)
-
-
-def structList(fp, obj, ind=""):
-    ind = ind + "  "
-    for fld in obj:
-        typ = type(fld)
-        print(ind, typ, file=fp)
-
-        if typ in iter(functions):
-            functions[typ](fp, fld, ind)
-
-
-functions = {dict: structDict,
-             list: structList
-             }
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
     #    execute(dumpReport, "EBFN", "WAPI")
     #    execute(dumpReport, "8660", "ACCU")
-    for f in latest():
-        inpFile = "output/" + f
-        outFile = inpFile.replace("output/", "struct/").replace(".json", ".txt")
-        resp = (wf.fetchFile(inpFile))
-
-        with open(outFile, "w") as fp:
-            typ = type(resp)
-            print("response", typ, file=fp)
-
-            functions[typ](fp, resp)
-            fp.close()
-
-
+    execute(dumpReport, "8660")
+    #    wf.getStructure(loc="8860")
+    wf.loop("8660", "OWMP", "forecast")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
